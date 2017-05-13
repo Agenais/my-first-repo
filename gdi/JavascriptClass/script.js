060c0ec74c34059caff7d6bd2150b2089f809ce5
Agenais <cacophony667@gmail.com>
2017-05-13 14:06:29 -0600

    changed code

--- gdi/JavascriptClass/script.js
+++ gdi/JavascriptClass/script.js
@@ -1,3 +1,48 @@
-alert('Hello World!'); //makes a popup
-console.log('Secret message'); //writes to the console
-document.write('Writing on the page.'); /*writes to the html page*/
+///alert('Hello World!'); //makes a popup
+//console.log('Secret message'); //writes to the console
+//document.write('Writing on the page.'); /*writes to the html page*/
+
+/*var myVariable = 6;
+console.log(myVariable);*/
+
+/*var numberOfApples = 6;
+var numberOfOranges = 7;
+var numberOfFruits = numberOfApples + numberOfOranges;
+console.log(numberOfFruits);
+
+
+
+var billAmount = 57.83;
+var tipPercentage = .15;
+var tipAmount = billAmount*tipPercentage;
+var totalBill = billAmount + tipAmount;
+document.write('Your total bill, with tip, is $' + totalBill.toFixed(2) + '.');
+
+function sentence() {
+    document.write("Hello this is my sentence.");
+}
+sentence();
+
+
+var firstName = 'Michelle';
+var lastName = ' Barnes';
+var wholeName = fullName(firstName, lastName);
+var firstName2 = 'Steve';
+var lastName2 = ' Rogers';
+var wholeName2 = fullName(firstName2, lastName2);
+function fullName(a, b) {
+  return a + b;
+}
+
+document.write(wholeName);
+document.write('<br>' + wholeName2);*/
+
+var temperature;
+function doINeedACoat(num) {
+  if (num < 50) {
+    document.write('Put on a coat');
+  }
+}
+doINeedACoat(49);
+doINeedACoat(60);
+doINeedACoat(30);
