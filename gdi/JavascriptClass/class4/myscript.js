1849444cec2d5497070c5a47aba07e89d50480f9
Agenais <cacophony667@gmail.com>
2017-05-20 15:01:32 -0600

    added new work

---
+++ gdi/JavascriptClass/class4/myscript.js
@@ -0,0 +1,38 @@
+function writeResults (result){
+    //First, find the empty div where we want to put the answers.
+    var resultsDiv = document.getElementById('resultsBox');
+
+    //Second, create a new paragraph
+    var newParagraph = document.createElement('p');
+
+    //Create a text node, a string of text, to put inside the paragraph.
+    var paragraphText = document.createTextNode(result);
+
+    //Now, we assemble our results. Put the new text in the new paragraph
+    newParagraph.appendChild(paragraphText);
+
+    //Then put it inside the div we found earlier
+    resultsDiv.appendChild(newParagraph);
+}
+
+writeResults('Hi there!');
+
+function changePicture () {
+  document.getElementById('turtlePic');
+  turtlePic.src = 'zebra.jpg';
+}
+
+function changePicture2 () {
+  document.getElementById('turtlePic');
+  turtlePic.src = 'turtle.jpg';
+}
+
+function addText (){
+  var mainDiv = document.getElementById('main');
+  var newParagraph = document.createElement('p');
+  var paragraphText = document.createTextNode('Here is an added paragraph.');
+  newParagraph.appendChild(paragraphText);
+  mainDiv.appendChild(newParagraph);
+}
+
+addText();
--- gdi/JavascriptClass/script.js
+++ gdi/JavascriptClass/script.js
@@ -37,12 +37,59 @@ function fullName(a, b) {
 document.write(wholeName);
 document.write('<br>' + wholeName2);*/
 
-var temperature;
+/*var temperature;
 function doINeedACoat(num) {
-  if (num < 50) {
-    document.write('Put on a coat');
+  if (num < 0) {
+    document.write('<br/>Stay inside.');
+  }
+  else if (num <= 50 && num >30) {
+    document.write('<br/>Put on a coat.');
+  }
+  else if (num <= 30 && num >=0) {
+    document.write('<br/>Put on a coat and a hat.');
+  }
+  else {
+    document.write('<br/>Wear whatever you want.');
   }
 }
-doINeedACoat(49);
+
 doINeedACoat(60);
 doINeedACoat(30);
+doINeedACoat(-1);
+doINeedACoat(49);*/
+
+/*function nineTimes() {
+for (var a = 1; a <= 12; a++) {
+  for (var b = 1; b <=12; b++) {
+    var c = a * b;
+    console.log(c);
+    }
+  }
+}
+
+nineTimes();*/
+
+//var favoriteFoods = ['Chiken Tikka Masala ','Eggplant Parmesean ','Cookies ','Nachos '];
+
+/*document.write(favoriteFoods[0]);
+document.write('<br/>');
+document.write(favoriteFoods[2]);*/
+
+/*for (var i = 0; i < favoriteFoods.length; i++) {
+  document.write(favoriteFoods[i]);
+}*/
+
+/*var recipe =
+{recipeTitle: "Basil Rice",
+servings: 4,
+ingredients: ["2 cups of rice","2 tbsp butter","2 tsp chicken bullion","1 dried tsp basil","1/2 tsp dried onion","1/4 tsp garlic powder","2 cups water"],
+directions: "Mix together all ingredients and microwave for 7 minutes.",
+letsCook: function() {
+  document.write("I\'m hungry! Let\'s cook " + recipe.recipeTitle + ". <br>");
+}
+};
+
+recipe.letsCook();
+
+
+document.write(recipe.recipeTitle + ' has ' + recipe.servings + " servings.");*/
